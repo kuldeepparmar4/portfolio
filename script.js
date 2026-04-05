278e36ca8acde0f18087c26e6aa90add884eb604
Your Name <kuldeepparmar4>
2026-04-05 16:47:54 +0530

    Initial portfolio upload

---
+++ script.js
@@ -0,0 +1,66 @@
+window.addEventListener("scroll", function () {
+  const header = document.getElementById("header");
+
+  if (window.scrollY > 50) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
+
+const words = ["Designer", "Developer"];
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+const typingElement = document.querySelector(".typing");
+
+function typeEffect() {
+  const currentWord = words[wordIndex];
+
+  if (!isDeleting) {
+    typingElement.textContent = currentWord.substring(0, charIndex + 1);
+    charIndex++;
+
+    if (charIndex === currentWord.length) {
+      setTimeout(() => (isDeleting = true), 1000);
+    }
+  } else {
+    typingElement.textContent = currentWord.substring(0, charIndex - 1);
+    charIndex--;
+
+    if (charIndex === 0) {
+      isDeleting = false;
+      wordIndex = (wordIndex + 1) % words.length;
+    }
+  }
+
+  setTimeout(typeEffect, isDeleting ? 80 : 120);
+}
+
+if (typingElement) {
+  typeEffect();
+}
+
+// Initialize EmailJS
+(function () {
+  emailjs.init("LMELVfqEFvATSjdl-");
+})();
+
+// Form Submit
+document
+  .getElementById("contact-form")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    emailjs.sendForm("service_kpwebsite1", "template_7h4clx6", this).then(
+      function () {
+        alert("Message Sent Successfully ");
+        document.getElementById("contact-form").reset();
+      },
+      function (error) {
+        alert("Failed to send ");
+        console.log(error);
+      },
+    );
+  });
